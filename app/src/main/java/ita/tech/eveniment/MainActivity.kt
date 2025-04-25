580f0634bf64312254dc4d33fc346ffe2df7ff4a
Rogelio Glez <rogelio.glz87@gmail.com>
2025-04-25 09:29:05 -0600

    [Roger] Se agrega encendido automatico de la app.

--- app/src/main/java/ita/tech/eveniment/MainActivity.kt
+++ app/src/main/java/ita/tech/eveniment/MainActivity.kt
@@ -1,9 +1,13 @@
 package ita.tech.eveniment
 
 import android.Manifest
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -17,8 +21,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontVariation
 import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
+import ita.tech.eveniment.broadcast.BootCompletedReceiver
 import ita.tech.eveniment.navegation.NavManager
 import ita.tech.eveniment.socket.SocketHandler
 import ita.tech.eveniment.ui.theme.EvenimentTheme
@@ -28,6 +34,7 @@ import ita.tech.eveniment.viewModels.RecursoVideoModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,19 +49,12 @@ class MainActivity : ComponentActivity() {
              * Permisos para la lectura de archivos Locales
              */
             var permission by remember {
-                mutableStateOf(
-                    ContextCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                    ) == PackageManager.PERMISSION_GRANTED
-                )
+                mutableStateOf(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED )
             }
 
             val permissionLaucher = rememberLauncherForActivityResult(
                 contract = ActivityResultContracts.RequestPermission(),
-                onResult = { isGranted ->
-                    permission = isGranted
-                }
+                onResult = { isGranted -> permission = isGranted }
             )
 
             LaunchedEffect(key1 = true) {
@@ -74,4 +74,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
